90782705a171550ca39d76e7e7df256531b27689
Shtolz1983 <denmifi@gmail.com>
2020-07-26 22:34:16 +0300

    ‘add'

--- bot.py
+++ bot.py
@@ -1,15 +1,22 @@
 import telebot
-import config
+import datetime
+import requests
+from apscheduler.schedulers.blocking import BlockingScheduler
+sched = BlockingScheduler()
 
+date =str(datetime.datetime.today().date())
+url = "https://api.nasa.gov/planetary/apod"
 
+payload = {'date': date, 'hd': 'bool', 'api_key': 'ulpmHwaZnRbTY22axukFIXy4Uc2qKAPKUlnCkv7C'}
+response = requests.request("GET", url, params=payload)
 
-bot = telebot.TeleBot(config.token)
+url_bot = response.json()['url']
+text_bot =response.json()['explanation']
+bot = telebot.TeleBot('1073569376:AAFXWbFgho9Q3r4zODyDqWvSvuLg2bIUPCI')
 id = '-1001463284489'
-@bot.message_handler(content_types=['text'])
-def repeat_all_messages(message):
-    bot.send_message('-1001463284489', 'example')
 
-bot.send_photo('-1001463284489', 'https://apod.nasa.gov/apod/image/2006/EuropaJupiter_Voyager_2792.jpg')
+@sched.scheduled_job('cron', day_of_week ='mon-sun' , hour=12)
+def job():
+    return bot.send_photo(id, url_bot), bot.send_message(id, text_bot)
+sched.start()
 
-if __name__ == '__main__':
-    bot.infinity_polling()
