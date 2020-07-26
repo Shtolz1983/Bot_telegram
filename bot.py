f82ba5534b8b11ac2135dd1252b3cac8efbfbfb4
Shtolz1983 <denmifi@gmail.com>
2020-07-26 22:56:04 +0300

    'add'

--- bot.py
+++ bot.py
@@ -7,13 +7,13 @@ sched = BlockingScheduler()
 date =str(datetime.datetime.today().date())
 url = "https://api.nasa.gov/planetary/apod"
 
-payload = {'date': date, 'hd': 'bool', 'api_key': 'ulpmHwaZnRbTY22axukFIXy4Uc2qKAPKUlnCkv7C'}
+payload = {'date': date, 'hd': 'bool', 'api_key': '3'}
 response = requests.request("GET", url, params=payload)
 
 url_bot = response.json()['url']
 text_bot =response.json()['explanation']
-bot = telebot.TeleBot('1073569376:AAFXWbFgho9Q3r4zODyDqWvSvuLg2bIUPCI')
-id = '-1001463284489'
+bot = telebot.TeleBot('3')
+id = '3'
 
 @sched.scheduled_job('cron', day_of_week ='mon-sun' , hour=12)
 def job():
